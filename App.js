a65fe7969cce17f5d40a5606147a5bae27b9fb41
rivan <rivanandfriends23@gmail.com>
2025-05-10 02:22:12 +0700

    change app.js hello world 4

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Text } from 'react-native';
 
 const App = () => {
-  return <Text>Hello World 3</Text>;
+  return <Text>Hello World 4</Text>;
 }
  
 export default App;
