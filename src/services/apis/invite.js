2ef8c5ca777d3c0afb1eb778b44d21994b5b0b41
rivan <rivanandfriends23@gmail.com>
2025-07-08 18:30:41 +0700

    invite new

--- src/services/apis/invite.js
+++ src/services/apis/invite.js
@@ -14,7 +14,7 @@ export const getEmailVerification = async() => {
 export const postForgotPassword = async(body) => {
   const {email} = body || {}
   try {
-    const res = await vercelInstance.post('/invite//send-email-forgot-password', {email});
+    const res = await vercelInstance.post('/invite/send-email-forgot-password', {email});
     return formatResponse({ res, isToastSuccess: true });
   } catch (error) {
     return formatResponse({ res: error?.response, isToastError: true });
