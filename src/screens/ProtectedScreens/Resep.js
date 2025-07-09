233ab888f31fcf02df7b44fa2748c76fbe689cb7
rivan <rivanandfriends23@gmail.com>
2025-07-10 05:22:36 +0700

    new molecules,Homepageicon(molecules),profilelist(molecules),tampilan new( homepage,formtambahanak,formtambahteam,informasi,team posyandu,profile)

--- src/screens/ProtectedScreens/Resep.js
+++ src/screens/ProtectedScreens/Resep.js
@@ -2,14 +2,15 @@ import React from 'react'
 import Container from '../../atomic/atoms/Container';
 import Header from '../../atomic/molecules/Header';
 
-const RiwayatBalita = () => {
+const Resep = () => {
     return ( 
         <Container useEarlyReturn useSafeArea>
-        <Container usePaddingHorizontal>
-            
-        </Container>
+            <Header useBack title='Resep'/>
+            <Container  usePaddingHorizontal>
+                
+            </Container>
         </Container>
      );
 }
  
-export default RiwayatBalita;
+export default Resep;
